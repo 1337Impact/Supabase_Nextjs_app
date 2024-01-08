7c69d6f8297cc3a8d401def3877f5db972921b24
Mohammed Benkhattab <mobenkhattab@gmail.com>
2024-01-08 17:02:43 +0100

    fix other users can view edit button

--- app/Businesses.tsx
+++ app/Businesses.tsx
@@ -26,11 +26,11 @@ export default function Businesses({ businesses }: { businesses: Business[] }) {
       {data?.map((business) => (
         <li
           key={business.id}
-          className="border-2 border-white rounded-md p-2 mt-2 bg-gray-700 shadow-md shadow-gray-400"
+          className="border-2 border-white rounded-md p-3 mt-2 bg-gray-700 shadow-md shadow-gray-400"
         >
           <h1 className="text-lg font-semibold">{business.name}</h1>
           <h2 className="text-sm text-gray-300 mt-1">{business.email}</h2>
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between items-center mt-1">
             <p className="text-sm text-gray-300">
               {new Date(business.created_at).toLocaleDateString("en-US", {
                 weekday: "long",
@@ -39,13 +39,15 @@ export default function Businesses({ businesses }: { businesses: Business[] }) {
                 year: "numeric",
               })}
             </p>
-            <Link
-              type="button"
-              href={`/edit/${business.id}`}
-              className="border-2 border-gray-100 rounded-md py-1 px-3 float-right"
-            >
-              edit
-            </Link>
+            {business.user_id === user?.id && (
+              <Link
+                type="button"
+                href={`/edit/${business.id}`}
+                className="border-2 border-gray-100 rounded-md py-1 px-3 float-right"
+              >
+                edit
+              </Link>
+            )}
           </div>
         </li>
       ))}
